4d82d725a6db6e3318a0fad990fdb438d9e176ec
Nandom <nandommamdam@gmail.com>
2022-05-06 06:02:38 +0100

    cofigure server for heroku

--- client/src/components/index.jsx
+++
@@ -1,3 +0,0 @@
-export { default as Header } from './Header/Header';
-export { default as ProfileDetails } from './ProfileDetails/ProfileDetails';
-export { default as RepositoriesDetails } from './RepositoriesDetails/RepositoriesDetails';
